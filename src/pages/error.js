14739475d3d99a8497ff8480739e95e8369e968b
siavhnz <siavhnz@gmail.com>
2023-05-14 03:14:21 +0330

    Add error page style

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,6 +1,7 @@
 import { useRouteError } from "react-router-dom";
 import { Header, Main } from "../components/Layout";
 import Wrapper from "../components/Layout/Wrapper";
+import styles from "./error.module.css";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -9,11 +10,15 @@ export default function ErrorPage() {
     <>
         <Header />
         <Main>
-            <Wrapper>
+            <Wrapper cssClass={styles.container}>
                 <h1>Oops!</h1>
                 <p>Sorry, an unexpected error has occurred.</p>
                 <p>
                     <i>{error.statusText || error.message}</i>
+                </p>
+                <p className={styles.vpn}>
+                  Attention:
+                  If you call the site from countries like Iran, Syria, etc, Please Run a VPN
                 </p>
             </Wrapper>
         </Main>
